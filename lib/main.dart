87e67b22bc93b2ac5b2e8689961d6e98817f51ee
mosheee <moshe.braun07@gmail.com>
2021-07-19 18:09:19 +0300

    adding notes

--- lib/main.dart
+++ lib/main.dart
@@ -22,51 +22,54 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List groupsForUi = [];
-  int nextGroup = 0;
+  int nextGroup = 1;
   final List keepingTheGroupsFromServer = [
     [1, 2, 3, 4, 5],
-    [6, 7],
-    [8, 9, 10]
+    [6, 7, 8],
+    [9, 10, 11, 12]
   ];
-  // ignore: close_sinks
-  StreamController? representStreamControllerFromServer;
-  StreamSubscription? representStreamSubscriptionFromServer;
-  //
+  Future<String>? _future;
   // ignore: close_sinks
   StreamController? _streamControllerForUi;
   Stream? _streamForUi;
+
+  Future<String> initialDataFromServer() {
+    return Future.delayed(Duration(seconds: 3)).then((value) {
+      for (int oneGroup in keepingTheGroupsFromServer[0]) {
+        groupsForUi.insert(0, oneGroup);
+      }
+      _streamControllerForUi!.add(groupsForUi.last);
+      return 'first http request is complete';
+    });
+  }
 
   manageSwipe(bool swipe) {
     print('group number got $swipe');
     groupsForUi.removeLast();
     groupsForUi.isNotEmpty
         ? _streamControllerForUi!.add(groupsForUi.last)
-        : _streamControllerForUi!.add(null);
+        : _streamControllerForUi!.add('show circular');
+  }
+
+  callForMoreGroups() async {
+    List<int> anotherListOfGroups =
+        await Future.delayed(Duration(seconds: 3), () {
+          print('future is done');
+      return keepingTheGroupsFromServer[nextGroup];
+    });
+    bool updateUi = groupsForUi.isEmpty;
+    for (int oneGroup in anotherListOfGroups) {
+      groupsForUi.insert(0, oneGroup);
+    }
+    _streamControllerForUi!.add(groupsForUi.last);
+    print(groupsForUi);
+    nextGroup++;
   }
 
   @override
   void initState() {
     super.initState();
-    // there are two stream :
-    // 1 - stream that represent the stream from server
-    // 2 - stream that manage the ui
-    // ignore: close_sinks
-    // this stream is represent the stream coming from the server
-    representStreamControllerFromServer = StreamController();
-    representStreamSubscriptionFromServer =
-        representStreamControllerFromServer!.stream.listen((event) {
-      if (event is List<int>) {
-        bool? updateUi = groupsForUi.isEmpty;
-        for (int group in event) {
-          groupsForUi.insert(0, group);
-        }
-        updateUi ? _streamControllerForUi!.add(groupsForUi.last) : null;
-//        _streamControllerForUi!.add(event);
-      } else {
-        print('event isnt list, event is : $event');
-      }
-    });
-
+    _future = initialDataFromServer();
     _streamControllerForUi = StreamController();
     _streamForUi = _streamControllerForUi!.stream;
   }
@@ -74,51 +77,44 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            child: GestureDetector(
-              child: Text(
-                'add another group to list (function as the stream from server)',
-                style: TextStyle(fontSize: 20),
-              ),
-              onTap: () {
-                representStreamControllerFromServer!
-                    .add(keepingTheGroupsFromServer[nextGroup]);
-                // next line of code dont need to be used with the real serve
-                nextGroup < 3 ? nextGroup++ : null;
-              },
-            ),
-          ),
-          StreamBuilder(
-            stream: _streamForUi,
-            builder: (context, snap) {
-              print(snap.hasData);
-              print(snap.connectionState);
-              if (snap.hasData &&
-                  snap.connectionState == ConnectionState.active) {
-                return Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.blue,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      manageSwipe(true);
-                    },
-                    child: Text(snap.data!.toString()),
-                  ),
-                );
-              }
-              // i need to know how the server is behave for sabtitute this condition in a more resonable one
-              if (snap.data == null) {
-                return CircularProgressIndicator();
-              }
-              return CircularProgressIndicator();
-            },
-          ),
-        ],
+      body: Center(
+        child: FutureBuilder(
+          future: _future,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return StreamBuilder(
+                stream: _streamForUi,
+                builder: (context, snap) {
+                  print(snap.hasData);
+                  print(snap.connectionState);
+                  if (snap.hasData &&
+                      snap.connectionState == ConnectionState.active) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          manageSwipe(true);
+                          if (groupsForUi.length == 2) {
+                            callForMoreGroups();
+                          }
+                        },
+                        child: Text(snap.data!.toString()),
+                      ),
+                    );
+                  }
+                  // i need to know how the server is behave for sabtitute this condition in a more resonable one
+                  if (snap.data == 'show circular') {
+                    return CircularProgressIndicator();
+                  }
+                  return CircularProgressIndicator();
+                },
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
